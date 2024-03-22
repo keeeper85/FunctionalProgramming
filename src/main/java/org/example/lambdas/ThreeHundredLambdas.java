189c4768c9e0de346729a186b5c03c1916307a09
keeeper85 <122378010+keeeper85@users.noreply.github.com>
2024-03-22 10:36:24 +0800

    Add 11 lambda heroes

--- src/main/java/org/example/lambdas/ThreeHundredLambdas.java
+++ src/main/java/org/example/lambdas/ThreeHundredLambdas.java
@@ -1,48 +1,92 @@
 package org.example.lambdas;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ThreeHundredLambdas {
 
     static int buffMultiplier = 1;
+    final static int bossHP = 10000;
+    final static int enemyArmySize = 100000;
 
     public static void main(String[] args) {
         //Zbliża się Armia Ciemności!
         ArmyOfDarkness armyOfDarkness = new ArmyOfDarkness();
 
-        LambdaWizard hero1 = (warcry, army) -> castAoeSpell(warcry, army.undeadSoldiers);
-        hero1.attackUndeadArmy("Deszcz meteorów!", armyOfDarkness);
+        //Naprzeciw armii ciemności staje 300 niezłomnych bohaterów Lambda!
+        List<LambdaHero> lambdaHeroes = new ArrayList<>();
 
+        LambdaWizard hero1 = (spell, army) -> castAoeSpell(spell, army.undeadSoldiers);
+            lambdaHeroes.add(hero1);
         LambdaWarrior hero2 = overlord -> overlord.attackOverlord(meleeAttack());
-        hero2.attackOverlord(armyOfDarkness);
-
+            lambdaHeroes.add(hero2);
         LambdaCleric hero3 = () -> buffUp();
-        hero3.buffAllies();
+            lambdaHeroes.add(hero3);
+        LambdaWarrior hero4 = overlord -> overlord.attackOverlord(meleeAttack());
+            lambdaHeroes.add(hero4);
+        LambdaWizard hero5 = (spell, army) -> castAoeSpell(spell, army.undeadSoldiers);
+            lambdaHeroes.add(hero5);
+        LambdaWizard hero6 = (spell, army) -> castAoeSpell(spell, army.undeadSoldiers);
+            lambdaHeroes.add(hero6);
+        LambdaWizard hero7 = (spell, army) -> castAoeSpell(spell, army.undeadSoldiers);
+            lambdaHeroes.add(hero7);
+        LambdaWarrior hero8 = overlord -> overlord.attackOverlord(meleeAttack());
+            lambdaHeroes.add(hero8);
+        LambdaCleric hero9 = () -> buffUp();
+            lambdaHeroes.add(hero9);
+        LambdaWizard hero10 = (spell, army) -> castAoeSpell(spell, army.undeadSoldiers);
+            lambdaHeroes.add(hero10);
+        LambdaWizard hero11 = (spell, army) -> castAoeSpell(spell, army.undeadSoldiers);
+            lambdaHeroes.add(hero11);
 
-        nextHero();
 
+
+        standYourGround(lambdaHeroes, armyOfDarkness);
+        finalReport(armyOfDarkness, lambdaHeroes);
+        nextHeroes();
     }
 
-    interface LambdaWizard{
-        void attackUndeadArmy(String spellName, ArmyOfDarkness army);
+    private static void standYourGround(List<LambdaHero> lambdaHeroes, ArmyOfDarkness armyOfDarkness) {
+        for (LambdaHero lambdaHero : lambdaHeroes) {
+            if (lambdaHero instanceof LambdaWizard){
+                LambdaWizard wizard = (LambdaWizard) lambdaHero;
+                wizard.attackUndeadArmy(randomSpell(), armyOfDarkness);
+            }
+            else if (lambdaHero instanceof LambdaWarrior){
+                LambdaWarrior warrior = (LambdaWarrior) lambdaHero;
+                warrior.attackOverlord(armyOfDarkness);
+            }
+            else{
+                LambdaCleric cleric = (LambdaCleric) lambdaHero;
+                cleric.buffAllies();
+            }
+        }
     }
 
-    interface LambdaWarrior{
+
+    interface LambdaWizard extends LambdaHero{
+        void attackUndeadArmy(Spell spell, ArmyOfDarkness army);
+    }
+
+    interface LambdaWarrior extends LambdaHero{
         int attackOverlord(ArmyOfDarkness overlord);
     }
 
-    interface LambdaCleric{
+    interface LambdaCleric extends LambdaHero{
         void buffAllies();
     }
 
+    interface LambdaHero{}
+
     static class ArmyOfDarkness{
-        int lichOverlordHealth = 10000;
+        int lichOverlordHealth = bossHP;
         Queue<UndeadSoldier> undeadSoldiers = new LinkedList<>();
 
         public ArmyOfDarkness() {
-            for (int i = 0; i < 100000; i++) {
+            for (int i = 0; i < enemyArmySize; i++) {
                 undeadSoldiers.add(new UndeadSoldier());
             }
             System.out.println("Król Lich: Nikt nie pokona mojej armii ciemności!");
@@ -58,7 +102,7 @@ public class ThreeHundredLambdas {
     static class UndeadSoldier{}
 
     static int random(){
-        return buffMultiplier * ThreadLocalRandom.current().nextInt(0, 100);
+        return buffMultiplier * ThreadLocalRandom.current().nextInt(1, 100);
     }
 
     static void report(Target target, int damage){
@@ -66,7 +110,17 @@ public class ThreeHundredLambdas {
         else System.out.println(" -->Armia ciemności straciła " + damage + " nieumarłych żołnierzy!");
     }
 
-    static void castAoeSpell(String spellName, Queue<UndeadSoldier> army){
+    static void finalReport(ArmyOfDarkness army, List<LambdaHero> heroes){
+        System.out.println();
+        int fallenUndead = enemyArmySize - army.undeadSoldiers.size();
+        System.out.println("Walka dobiegła końca. Armia ciemności straciła " + fallenUndead + " nieumarłych żołnierzy.");
+        int bossHpLeft = army.lichOverlordHealth;
+        System.out.println("Królowi Lich zostało " + bossHpLeft + " punktów życia i musiał uciekać, a jego armia popadła w rozsypkę.");
+        int fallenHeroes = heroes.size();
+        System.out.println(fallenHeroes + " dzielnych bohaterów walczyło i oddało życie za naszą wolność. Niech ich pamięć trwa na wieki!");
+    }
+
+    static void castAoeSpell(Spell spellName, Queue<UndeadSoldier> army){
         int aoe = random();
         System.out.print("Jeden z czarodziejów rzuca czar: " + spellName);
         for (int i = 0; i < aoe; i++) {
@@ -85,17 +139,27 @@ public class ThreeHundredLambdas {
         buffMultiplier++;
     }
 
-    static void nextHero(){
-        int random = random();
-        Hero hero = Hero.CZARODZIEJ;
-        if (random > 50 && random < 90) hero = Hero.WOJOWNIK;
-        else if (random >= 90) hero = Hero.KAPŁAN;
+    static Spell randomSpell(){
+        int random = ThreadLocalRandom.current().nextInt(0, Spell.values().length);
+        return Spell.values()[random];
+    }
+
+    static void nextHeroes(){
         System.out.println("--------------");
-        System.out.println("Kolejny bohater: " + hero);
+        for (int i = 0; i < 5; i++) {
+            int random = ThreadLocalRandom.current().nextInt(0, 100);;
+            Hero hero = Hero.CZARODZIEJ;
+            if (random > 50 && random < 80) hero = Hero.WOJOWNIK;
+            else if (random >= 80 && random < 94) hero = Hero.KAPŁAN;
+            else if (random >= 94) hero = Hero.ŁOTRZYK;
+            System.out.println("Kolejny bohater: " + hero);
+        }
     }
 
     enum Target{OVERLORD, SOLDIER}
 
-    enum Hero{CZARODZIEJ, WOJOWNIK, KAPŁAN}
+    enum Hero{CZARODZIEJ, WOJOWNIK, KAPŁAN, ŁOTRZYK}
+
+    enum Spell{Deszcz_meteorów, Kula_ognia, Maelstorm, Trzęsienie_ziemi, Wyładowanie_łańcuchowe, Eksplozja, Implozja, Piorun_kulisty, Śnieżyca, Wulkan, Gejzer, Ognista_chmura, Gradobicie}
 
 }
